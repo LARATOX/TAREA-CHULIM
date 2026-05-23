9fda30a10a4152f98603e9ea3ae7145260a9cdc5
Cristian <cristian.abad700@gmail.com>
2026-05-23 18:07:06 -0500

    se agregaron 2 enums para la Categoria de Ingresos y otro para la Categoria de Gastos

---
+++ finanzaz/src/CategoriaGastos.kt
@@ -0,0 +1,10 @@
+enum class CategoriaGastos {
+    Transporte,
+    Comida,
+    Renta,
+    Pago_Servicios_Luz_Agua_Internet_etc,
+    Suscripcion_Plataforma_Digital,
+    Ropa,
+    Pago_Colegiatura,
+    Emergencia_Medica
+}
---
+++ finanzaz/src/CategoriaIngresos.kt
@@ -0,0 +1,8 @@
+//Define la categorias posible para los ingresos y gastos
+enum class CategoriaIngresos {
+    Mesada,
+    Beca,
+    Sueldo,
+    Venta_de_servicio,
+    Venta_de_bienes
+}
