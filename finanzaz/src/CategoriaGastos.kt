5c37a44d5838c5ea9c265f5223625ee09d080263
Cristian <cristian.abad700@gmail.com>
2026-05-23 19:04:13 -0500

    se agrego un texto para cada categoria de Ingresos y Gastos

--- finanzaz/src/CategoriaGastos.kt
+++ finanzaz/src/CategoriaGastos.kt
@@ -1,10 +1,15 @@
-enum class CategoriaGastos {
-    Transporte,
-    Comida,
-    Renta,
-    Pago_Servicios_Luz_Agua_Internet_etc,
-    Suscripcion_Plataforma_Digital,
-    Ropa,
-    Pago_Colegiatura,
-    Emergencia_Medica
+enum class CategoriaGastos(private val texto: String) {
+    Transporte("Transporte"),
+    Comida("Comida"),
+    Renta("Renta"),
+    Pago_Servicios("Pago de Servicios Basicos (Luz, Agua, Internet ...)"),
+    Suscripcion_Plataforma("Suscripcion Plataforma (Spotify, Netflix, ChatGPT ...)"),
+    Ropa("Ropa"),
+    Pago_Colegiatura("Pago de Colegiatura"),
+    Emergencia_Medica("Emergencia Medica");
+
+    override fun toString(): String {
+        return texto
+    }
+
 }
--- finanzaz/src/CategoriaIngresos.kt
+++ finanzaz/src/CategoriaIngresos.kt
@@ -1,8 +1,12 @@
 //Define la categorias posible para los ingresos y gastos
-enum class CategoriaIngresos {
-    Mesada,
-    Beca,
-    Sueldo,
-    Venta_de_servicio,
-    Venta_de_bienes
+enum class CategoriaIngresos(val texto: String) {
+    Mesada("Mesada"),
+    Beca("Beca"),
+    Sueldo("Sueldo Recibido"),
+    Venta_de_servicio("Venta de servicios"),
+    Venta_de_bienes("Venta de Bienes");
+
+    override fun toString(): String {
+        return texto
+    }
 }
